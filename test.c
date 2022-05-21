7840d27b18d7c2980954bcd5253d392504342a05
Mike Klein <mtklein@gmail.com>
2022-05-21 10:47:34 -0500

    test.c style

--- test.c
+++ test.c
@@ -9,11 +9,11 @@ static void test(struct hash* (*fn)(struct hash*)) {
     free(fn(NULL));
 }
 
-static bool is_ctx(void* val, void* ctx) {
+static bool is_ctx(void *val, void *ctx) {
     return val == ctx;
 }
 
-static struct hash* basics(struct hash* h) {
+static struct hash* basics(struct hash *h) {
     assert(!hash_lookup(h, 5, is_ctx, (void*)5));
     assert(!hash_lookup(h, 7, is_ctx, (void*)7));
 
@@ -33,8 +33,8 @@ static struct hash* basics(struct hash* h) {
     return h;
 }
 
-static struct hash* thorough(struct hash* h) {
-    const int K = 47;
+static struct hash* thorough(struct hash *h) {
+    int const K = 47;
     for (int i = 0; i < K; i++) {
         for (int j = 0; j < K; j++) {
             assert(hash_lookup(h, j, is_ctx, (void*)(uintptr_t)j) == (j<i));
@@ -44,13 +44,13 @@ static struct hash* thorough(struct hash* h) {
     return h;
 }
 
-static struct hash* null_val(struct hash* h) {
+static struct hash* null_val(struct hash *h) {
     h = hash_insert(h, 42, NULL);
     assert(hash_lookup(h, 42, is_ctx, NULL));
     return h;
 }
 
-static struct hash* zero_hash(struct hash* h) {
+static struct hash* zero_hash(struct hash *h) {
     h = hash_insert(h, 0, (void*)42);
     assert(hash_lookup(h, 0, is_ctx, (void*)42));
     return h;
